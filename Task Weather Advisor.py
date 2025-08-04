be322b7fd5244c8854f37e0e0cd8cd1caa685431
AboFahd2020 <cocodragon.marketing@gmail.com>
2025-08-04 13:00:20 +0300

    Add files via upload

---
+++ Task Weather Advisor.py
@@ -0,0 +1,12 @@
+temp = 32
+if temp >=30 :
+ print ("It's a hot day. Stay hydrated!")
+temp = 25
+if temp >=20 and temp <=30 :
+ print ("It's a warm day. Enjoy the weather!")
+temp = 15
+if temp >=10 and temp <=19 :
+ print ("It's a cool day. Wear a jacket!")
+temp = 8
+if temp < 10 :
+ print ("It's a cold day. Stay warm!")
